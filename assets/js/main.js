598e140e764dfc51f84f68227bb9c543f39dd3b8
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-17 13:47:19 +0200

    initial commit:setup

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+/* Consegna:
+1. L'utente clicca su un bottone che genererà una griglia di gioco quadrata (10 x 10). 
+    1.2. Ogni cella ha un numero progressivo, da 1 a 100.
+
+2. Quando l'utente clicca su ogni cella:  
+    2.1. La cella cliccata si colora di azzurro 
+    2.2. Viene emesso un messaggio in console con il numero della cella cliccata.
+*/
+
+/* 1. Creazione della griglia:
+    - creo un container nel markup
+    - inserisco nel container le celle tramite js
+    - ciclo la singola cella per creare la griglia
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 2. Aggiunta dell'event-listener al click:
+    - creo il bottone nel markup
+    - btn.addEventListener (click) --- si genera la griglia
+    - cell.addEventlister (click) -- in console n. della cella (ciclo for)
+
+*/
+
